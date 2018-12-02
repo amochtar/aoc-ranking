c79bc6616be9f99d923afdfd26d3370acfe67bb3
Adé Mochtar <amochtar@xebia.com>
2018-12-02 09:13:23 +0100

    Don't plot for days that are not available yet

--- calculate.js
+++ calculate.js
@@ -59,42 +59,56 @@ function calculateRanking(data) {
   // max score per star is number of members
   max_score = members.length
 
+  max_days = Object.values(data.members).reduce(function(acc, m) {
+      l = Object.values(m.completion_day_level).length;
+      if (l > acc) {
+        return l;
+      }
+      return acc;
+    }, 0)
+
   // do actual calculation of scores, ranks and stars
   for (let d = 0; d < 25; d++) {
-    for (let l = 1; l <= 2; l++) {
-      members.sort(compareMembersByDayLevel(d+1, l))
-      members.forEach(function (member, index) {
-        if (ranking[member.id].scores[d] === undefined) {
-          ranking[member.id].scores[d] = 0
-        }
+    date = Date.UTC(data.event, 11, d+1, 5)
+    if (d < max_days || date < new Date().getTime()) {
+      for (let l = 1; l <= 2; l++) {
+        members.sort(compareMembersByDayLevel(d+1, l))
+        members.forEach(function (member, index) {
+          if (ranking[member.id].scores[d] === undefined) {
+            ranking[member.id].scores[d] = 0
+          }
 
-        if (ranking[member.id].stars[d] === undefined) {
-          ranking[member.id].stars[d] = 0
-        }
+          if (ranking[member.id].stars[d] === undefined) {
+            ranking[member.id].stars[d] = 0
+          }
 
-        completed = getMemberCompletionDayLevel(member, d+1, l)
-        if (completed !== null) {
-          ranking[member.id].stars[d] = l
-          score = max_score - index
-          ranking[member.id].scores[d] += score
-          ranking[member.id].total_score += score
-          ranking[member.id].ranks[d] = index + 1
-        } else {
-          ranking[member.id].ranks[d] = 0
-        }
+          completed = getMemberCompletionDayLevel(member, d+1, l)
+          if (completed !== null) {
+            ranking[member.id].stars[d] = l
+            score = max_score - index
+            ranking[member.id].scores[d] += score
+            ranking[member.id].total_score += score
+            ranking[member.id].ranks[d] = index + 1
+          } else {
+            ranking[member.id].ranks[d] = 0
+          }
 
-        ranking[member.id].total_scores[d] = ranking[member.id].total_score
-      })
+          ranking[member.id].total_scores[d] = ranking[member.id].total_score
+        })
+      }
     }
   }
   ranking = Object.values(ranking)
 
   // finally get total_ranks for all days
   for (let d = 0; d < 25; d++) {
-    ranking.sort(compareRankingByDay(d))
-    ranking.forEach(function(rank, index) {
-      ranking[index].total_ranks[d] = index + 1
-    })
+    date = Date.UTC(data.event, 11, d+1, 5)
+    if (d < max_days || date < new Date().getTime()) {
+      ranking.sort(compareRankingByDay(d))
+      ranking.forEach(function(rank, index) {
+        ranking[index].total_ranks[d] = index + 1
+      })
+    }
   }
 
   return {ranking: ranking}
