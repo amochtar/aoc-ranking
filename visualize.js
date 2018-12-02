60d20146e20c3a07db679b65551f29d4dc03c91d
Adé Mochtar <amochtar@xebia.com>
2018-12-02 09:16:57 +0100

    * Better highlight of medals (top 3 fastest to get both stars) per day * Update d3 version * Some other small visual tweaks

--- visualize.js
+++ visualize.js
@@ -1,14 +1,9 @@
-const DIMENSIONS = getWindowDimensions();
-const WIDTH = DIMENSIONS.width;
-// const HEIGHT = DIMENSIONS.height - 100;
+const WIDTH = Math.max(1120, window.getComputedStyle(document.body).width.replace("px", ""));
 
 const INSETS = {'left': 275, 'right': 300, 'top': 30, 'bottom': 30};
 const PADDING = {'left': 20, 'right': 20, 'top': 15, 'bottom': 15};
 
 const TICK_MARK_LENGTH = 8;
-
-const MEDAL_RADIUS = 7;
-const INCOMPLETE_RADIUS = 5;
 
 const SCALES = {};
 
@@ -16,7 +11,6 @@ var DIMMED_OPACITY = 0.2;
 var HIGHLIGHT_OPACITY = 1.0;
 
 const DAY_COUNT = 25
-const MAX_ROWS = 100
 const ROW_HEIGHT = 35
 
 function visualize(data) {
@@ -24,7 +18,7 @@ function visualize(data) {
     if (chart === null) {
         chart = document.createElement("div")
         chart.setAttribute("id", "chart")
-        chart.setAttribute("style", "width: 100vw; height: 100vh;")
+        chart.setAttribute("style", "width: 100%;")
         document.body.appendChild(chart)
     } else {
         while (chart.firstChild) {
@@ -33,7 +27,6 @@ function visualize(data) {
     }
 
     var vis = d3.select(chart)
-        .attr('style', 'width: 100vw')
         .append('svg:svg')
         .attr('width', '100%')
         .attr('height', data.ranking.length*ROW_HEIGHT)
@@ -42,34 +35,58 @@ function visualize(data) {
 
     addDayTickLines(vis, DAY_COUNT);
 
-    addLabels(vis, DAY_COUNT, SCALES.y.range()[0] - PADDING.bottom, '0.0em', 'top');
+    addLabels(vis, DAY_COUNT, SCALES.y.range()[0] - PADDING.bottom, '0.0em');
 
     addRankingLines(vis, data);
 
-    addNameLabels(vis, data, 'pole', SCALES.x(0) - PADDING.right, 'end')
+    addNameLabels(vis, data, 'left', SCALES.x(0) - PADDING.right, 'end')
         .attr('y', function (d) {
             return SCALES.y(d.total_ranks[0]-1);
         });
-    addNameLabels(vis, data, 'flag', SCALES.x(DAY_COUNT-1) + PADDING.left, 'start')
+    addNameLabels(vis, data, 'right', SCALES.x(DAY_COUNT-1) + PADDING.left, 'start')
             .attr('y', function (d, i) {
             return SCALES.y(i);
         });
 
+    addProgress(vis, data);
     addMedals(vis, data);
 
     return data
 }
 
-function configureScales(data) {
-    SCALES.x = d3.scale.linear()
-        .domain([0, DAY_COUNT-1])
-        .range([INSETS.left, WIDTH - INSETS.right]);
+function getTextWidth(text) {
+    var context = getTextWidth.context
+    if (context === undefined) {
+        canvas = document.createElement("canvas")
+        context = canvas.getContext("2d");
+        bodyStyle = getTextWidth.bodyStyle || (getTextWidth.bodyStyle = window.getComputedStyle(document.body))
+        context.font = bodyStyle.fontSize + ' ' + bodyStyle.fontFamily;
+        getTextWidth.context = context
+    }
+    var metrics = context.measureText(text);
+    return metrics.width;
+}
 
-    SCALES.y = d3.scale.linear()
+function configureScales(data) {
+    labelWidth = 50 + data.ranking.reduce((
+        (acc, m) => {
+            w = getTextWidth(m.name);
+            if (w > acc) {
+                return w
+            }
+            return acc
+        }
+    ), 0)
+    SCALES.x = d3.scaleLinear()
+        .domain([0, DAY_COUNT-1])
+        .range([labelWidth, WIDTH - labelWidth]);
+
+    SCALES.y = d3.scaleLinear()
         .domain([0, data.ranking.length-1])
         .range([INSETS.top, data.ranking.length*ROW_HEIGHT - INSETS.bottom]);
 
-    SCALES.clr = d3.scale.category20();
+    schemeCategory20=["#1f77b4","#aec7e8","#ff7f0e","#ffbb78","#2ca02c","#98df8a","#d62728","#ff9896","#9467bd","#c5b0d5","#8c564b","#c49c94","#e377c2","#f7b6d2","#7f7f7f","#c7c7c7","#bcbd22","#dbdb8d","#17becf","#9edae5"]
+    SCALES.clr = d3.scaleOrdinal(schemeCategory20);
 }
 
 function highlight(vis, id) {
@@ -87,6 +104,15 @@ function highlight(vis, id) {
             return HIGHLIGHT_OPACITY;
         });
 
+    vis.selectAll('.medal')
+        .style('opacity', function(d) {
+            return DIMMED_OPACITY;
+        });
+    vis.selectAll('.medal.m'+id)
+        .style('opacity', function(d) {
+            return HIGHLIGHT_OPACITY;
+        });
+
     vis.selectAll('text.label')
         .style('opacity', function(d) {
             return d.id == id ? HIGHLIGHT_OPACITY : DIMMED_OPACITY;
@@ -97,6 +123,8 @@ function unhighlight(vis) {
     vis.selectAll('polyline')
         .style('opacity', HIGHLIGHT_OPACITY);
     vis.selectAll('circle')
+        .style('opacity', HIGHLIGHT_OPACITY);
+    vis.selectAll('.medal')
         .style('opacity', HIGHLIGHT_OPACITY);
     vis.selectAll('text.label')
         .style('opacity', HIGHLIGHT_OPACITY);
@@ -120,11 +148,11 @@ function addDayTickLines(vis, dayCount) {
         });
 }
 
-function addLabels(vis, data, y, dy, cssClass) {
-    vis.selectAll('text.day.' + cssClass)
+function addLabels(vis, data, y, dy) {
+    vis.selectAll('text.day')
         .data(SCALES.x.ticks(data))
         .enter().append('svg:text')
-        .attr('class', 'day ' + cssClass)
+        .attr('class', 'day')
         .attr('x', function(d) {
             return SCALES.x(d);
         })
@@ -184,59 +212,27 @@ function addNameLabels(vis, data, cssClass, x, textAnchor) {
 }
 
 function addMedals(vis, data) {
+    var symbolGenerator = d3.symbol()
+    	.type(d3.symbolStar)
+        .size(150);
+    var pathData = symbolGenerator()
+
     data.ranking.forEach(function (member, idx) {
         vis.append("defs");
-        vis.selectAll("circle.medal.m" + member.id)
+        vis.selectAll("path.medal.m" + member.id)
             .data(member.ranks)
             .enter()
-            .append("svg:circle")
+            .append("path")
             .attr("class", "medal m" + member.id)
-            .attr("cx", function(d, i) {
-                return SCALES.x(i);
+            .attr('transform', function(d, i) {
+                return 'translate(' + SCALES.x(i) + ', ' + SCALES.y(member.total_ranks[i] - 1) + ')';
             })
-            .attr("cy", function(d, i) {
-                return SCALES.y(member.total_ranks[i] - 1);
-            })
-            .attr("r", function(d) {
-                if (0 < d && d <= 3) {
-                    return MEDAL_RADIUS
-                }
-                return INCOMPLETE_RADIUS
-            })
-            .attr("visibility", function(d, i) {
-                if ((0 < d && d <= 3) || member.stars[i] < 2) {
-                    return "visible"
-                }
-                return "hidden"
-            })
+            .filter(function(d) { return 0 < d && d <= 3 })
+            .attr('d', pathData)
             .style("stroke", function(d, i) {
-                if (0 < d && d <= 3) {
-                    return "#0f0f23"
-                }
-                if (d == 1) {
-                    return "gold"
-                }
-                else if (d == 2) {
-                    return "silver"
-                }
-                else if (d == 3) {
-                    return "#963"
-                }
-                return SCALES.clr(member.total_ranks[0]);
+                return "#0f0f23"
             })
             .style("fill", function(d, i) {
-                if (member.stars[i] == 0) {
-                    return "#0f0f23"
-                }
-                else if (member.stars[i] == 1) {
-                    gradId = "m" + member.id + "d" + i
-                    var grad = vis.select("defs").append("linearGradient").attr("id", gradId)
-                        .attr("x1", "0%").attr("x2", "100%").attr("y1", "100%").attr("y2", "100%");
-                    grad.append("stop").attr("offset", "47%").style("stop-color", SCALES.clr(member.total_ranks[0]));
-                    grad.append("stop").attr("offset", "47%").style("stop-color", "#0f0f23");
-
-                    return "url(#" + gradId + ")"
-                }
                 if (d == 1) {
                     return "gold"
                 }
@@ -246,7 +242,6 @@ function addMedals(vis, data) {
                 else if (d == 3) {
                     return "#963"
                 }
-                return SCALES.clr(member.total_ranks[0]);
             })
             .on('mouseover', function(d) {
                 highlight(vis, member.id);
@@ -257,27 +252,57 @@ function addMedals(vis, data) {
     });
 }
 
-function getWindowDimensions() {
+function addProgress(vis, data) {
+    data.ranking.forEach(function (member, idx) {
+        vis.append("defs");
+        vis.selectAll("circle.progress.m" + member.id)
+            .data(member.ranks)
+            .enter()
+            .append("svg:circle")
+            .attr("class", "progress m" + member.id)
+            .attr("cx", function(d, i) {
+                return SCALES.x(i);
+            })
+            .attr("cy", function(d, i) {
+                return SCALES.y(member.total_ranks[i] - 1);
+            })
+            .attr("r", 5)
+            .style("stroke", function(d, i) {
+                return SCALES.clr(member.total_ranks[0]);
+            })
+            .style("fill", function(d, i) {
+                if (member.stars[i] == 0) {
+                    return "#0f0f23"
+                }
+                else if (member.stars[i] == 1) {
+                    gradId = "m" + member.id + "d" + i
+                    var grad = vis.select("defs").append("linearGradient").attr("id", gradId)
+                        .attr("x1", "0%").attr("x2", "100%").attr("y1", "100%").attr("y2", "100%");
+                    grad.append("stop").attr("offset", "49%").style("stop-color", SCALES.clr(member.total_ranks[0]));
+                    grad.append("stop").attr("offset", "49%").style("stop-color", "#0f0f23");
 
-    var width = 630;
-    var height = 460;
-    if (document.body && document.body.offsetWidth) {
-
-        width = document.body.offsetWidth;
-        height = document.body.offsetHeight;
-    }
-
-    if (document.compatMode == 'CSS1Compat' && document.documentElement && document.documentElement.offsetWidth) {
-
-        width = document.documentElement.offsetWidth;
-        height = document.documentElement.offsetHeight;
-    }
-
-    if (window.innerWidth && window.innerHeight) {
-
-        width = window.innerWidth;
-        height = window.innerHeight;
-    }
-
-    return {'width': width, 'height': height};
+                    return "url(#" + gradId + ")"
+                }
+                return SCALES.clr(member.total_ranks[0]);
+            })
+            .style("visibility", function(d, i) {
+                if (0 < d && d <= 3) {
+                    return "hidden"
+                }
+                if (member.stars[i] < 2) {
+                    return "visible"
+                }
+                if (i == 0 || i == member.stars.length-1) {
+                    return "visible"
+                }
+                return "hidden"
+            })
+            .on('mouseover', function(d) {
+                highlight(vis, member.id);
+            })
+            .on('mouseout', function() {
+                unhighlight(vis);
+            });
+    });
 }
+
